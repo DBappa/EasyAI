82dd24ab6ef542d1745ee7e2fc361268f466cb1a
DBappa <bappadityadutta2024@gmail.com>
2025-10-14 11:04:13 +0530

    Clerk Integration and Hero Section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { ArrowRight } from "lucide-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
   return (
     <div
       className="fixed z-5 w-full backdrop-blur-2xl flex justify-between 
@@ -13,11 +16,22 @@ const Navbar = () => {
       <img
         src={assets.logo}
         alt="logo"
-        className="w-32 sm:w-44"
+        className="w-32 sm:w-44 cursor-pointer"
         onClick={() => navigate("/")}
       />
-      <button className="flex items-center gap-2 rounded-full 
-      text-sm cursor-pointer bg-blue-600 text-white px-10 py-2.5">Get Started <ArrowRight className="w-4 h-4"/></button>
+
+      {user ? (
+        <UserButton afterSignOutUrl="/" />
+      ) : (
+        <button
+          className="flex items-center gap-2 rounded-full 
+      text-sm cursor-pointer bg-blue-600 text-white px-10 py-2.5"
+          onClick={openSignIn}
+        >
+          Log In <ArrowRight className="w-4 h-4" />
+        </button>
+      )}
+      
     </div>
   );
 };
